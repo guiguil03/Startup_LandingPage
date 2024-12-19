1a9e8c6f6b8b0226e1f18216e64487ce06b7758e
guiguil03 <guillaumel1103@gmail.com>
2024-12-19 14:56:19 +0100

    fichier

--- Landing-Page/src/App.tsx
+++ Landing-Page/src/App.tsx
@@ -1,13 +1,13 @@
 import "./App.css";
-import Header from "./components/header";
-import Page from "./components/page";
-import Footer from "./components/footer";
-import Notion from "./components/notion";
-import Chat from "./components/chat";
-import Course from "./components/course";
-import Maps from "./components/maps";
-import Other from "./components/other";
-import Final from "./components/final";
+import Header from "../src/components/header";
+import Page from "../src/components/page";
+import Footer from "../src/components/footer";
+import Notion from "../src/components/notion";
+import Chat from "../src/components/chat";
+import Course from "../src/components/course";
+import Maps from "../src/components/maps";
+import Other from "../src/components/other";
+import Final from "../src/components/final";
 
 function App() {
   return (
